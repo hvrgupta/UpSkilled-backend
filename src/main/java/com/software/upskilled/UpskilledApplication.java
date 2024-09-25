063e29279aafc91ab146dfb5e05875b235125920
Harsh Gupta <hgupta7@umd.edu>
2024-09-25 01:34:30 -0400

    Let's go Hello World

---
+++ src/main/java/com/software/upskilled/UpskilledApplication.java
@@ -0,0 +1,13 @@
+package com.software.upskilled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpskilledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpskilledApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/software/upskilled/UpskilledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software.upskilled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpskilledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
