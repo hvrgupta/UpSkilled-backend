548e2619aff033b8d7e25828cfb7a18ccbce8658
Harsh Gupta <hgupta7@umd.edu>
2024-11-05 11:24:57 -0500

    Employee course auth

---
+++ src/main/java/com/software/upskilled/utils/EmployeeCourseAuth.java
@@ -0,0 +1,42 @@
+package com.software.upskilled.utils;
+
+import com.software.upskilled.Entity.Course;
+import com.software.upskilled.Entity.Users;
+import com.software.upskilled.service.CourseService;
+import com.software.upskilled.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeCourseAuth {
+
+    private final UserService userService;
+    private final CourseService courseService;
+
+    public EmployeeCourseAuth(UserService userService, CourseService courseService) {
+        this.userService = userService;
+        this.courseService = courseService;
+    }
+
+    public ResponseEntity<String> validateEmployeeForCourse(Long courseId, Authentication authentication) {
+        // Get the currently authenticated user (employee)
+
+
+        Course course = courseService.findCourseById(courseId);
+
+        if (course == null) {
+            return ResponseEntity.badRequest().body("Invalid course ID");
+        }
+
+        String email = authentication.getName();
+        Users employee = userService.findUserByEmail(email);
+
+        if (course.getEnrollments().stream().noneMatch(enrollment -> enrollment.getEmployee().equals(employee))) {
+            return ResponseEntity.status(403).body("You are not enrolled in this course");
+        }
+
+        return null;
+
+    }
+}
